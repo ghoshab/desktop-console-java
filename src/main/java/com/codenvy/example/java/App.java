ba157c0cd0ad484a15bb387a78e941b9f8afc729
ghoshab <51268137+ghoshab@users.noreply.github.com>
2019-06-01 23:44:42 +0530

    test again

--- src/main/java/com/codenvy/example/java/App.java
+++ src/main/java/com/codenvy/example/java/App.java
@@ -6,6 +6,6 @@ package com.codenvy.example.java;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Test!");
+        System.out.println("Test1!");
     }
 }
